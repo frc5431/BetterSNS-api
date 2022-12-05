d139660a98042312e1a1da316d8733c3148c992b
Rubyboat <rudy@rudysoliz.com>
2022-12-05 12:08:50 -0600

    initial commit

---
+++ api/controllers/pregame/get.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Get',
+
+
+  description: 'Get pregame.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+    
+    // All done.
+    return;
+
+  }
+
+
+};
---
+++ api/models/Pregame.js
@@ -0,0 +1,48 @@
+/**
+ * Pregame.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    name: {
+      type: "string"
+    },
+    id: {
+      type: "number",
+      columnName: '_id'
+    },
+    match: {
+      type: "number"
+    },
+    preload: {
+      type: "boolean"
+    },
+    human: {
+      type: "boolean"
+    },
+    noshow: {
+      type: "boolean"
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
